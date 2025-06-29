0848bd5be1dc71e55ab359f37778a3be00b5121a
kamil-tron <kamildiaz513@gmail.com>
2025-06-29 18:03:23 -0500

    cpp.ServicioVuelos comeback-

--- ServicioVuelos.h
+++ ServicioVuelos.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include "RepoVuelos.h"
 #include "RepoAsientos.h"
 #include "Lista.h"
@@ -10,107 +9,25 @@
 
 using namespace std;
 
-// Temporal para recoger el recorrido in-order
-static Lista<Vuelo>* listaTemporal = nullptr;
-
-// Inserta un vuelo en la lista temporal
-static void insertarEnListaTemporal(Vuelo v) {
-	if (listaTemporal) listaTemporal->agregaFinal(v);
-}
-
 class ServicioVuelos {
 private:
-	RepoVuelos repoVuelos;
-	RepoAsientos repoAsientos;
+    RepoVuelos repoVuelos;
+    RepoAsientos repoAsientos;
 
-	HashTable<int, Vuelo>* idx;
-	ArbolAVL<Vuelo>* vuelosPorFecha;
+    HashTable<int, Vuelo>* idx;
+    ArbolAVL<Vuelo>* vuelosPorFecha;
 
-	void cargarIndice() {
-		auto lista = repoVuelos.cargarTodos();
-		for (int i = 0; i < lista.longitud(); ++i) {
-			Vuelo v = lista.obtenerPos(i);
-			idx->insertar(v.getId(), v);
-			vuelosPorFecha->insertar(v);
-		}
-	}
+    void cargarIndice();
 
 public:
-	ServicioVuelos()
-		: idx(new HashTable<int, Vuelo>(2000, hashInt)),
-		vuelosPorFecha(new ArbolAVL<Vuelo>(insertarEnListaTemporal)) {
-		cargarIndice();
-	}
+    ServicioVuelos();
+    ~ServicioVuelos();
 
-	~ServicioVuelos() {
-		delete idx;
-		delete vuelosPorFecha;
-	}
+    Lista<Vuelo> listarVuelos() const;
+    Lista<Vuelo> listarVuelosPorFecha() const;
 
-	Lista<Vuelo> listarVuelos() const {
-		return repoVuelos.cargarTodos();
-	}
-
-	Lista<Vuelo> listarVuelosPorFecha() const {
-		Lista<Vuelo> ordenados;
-		listaTemporal = &ordenados;
-		vuelosPorFecha->inOrden();
-		listaTemporal = nullptr;
-		return ordenados;
-	}
-
-	bool buscarVuelo(int id, Vuelo& v) const {
-		const Vuelo* p = idx->obtener(id);
-		if (!p) return false;
-		v = *p;
-		return true;
-	}
-
-	bool crearVuelo(const Vuelo& v) {
-		repoVuelos.agregar(v);
-		idx->insertar(v.getId(), v);
-		vuelosPorFecha->insertar(v);
-
-		int total = v.getCapacidad();
-		int cutoff = total / 4;
-		int count = 0, fila = 1;
-
-		while (count < total) {
-			for (char letra = 'A'; letra <= 'F' && count < total; ++letra) {
-				bool vip = (count < cutoff);
-				repoAsientos.agregar(Asiento(v.getId(), fila, letra, false, vip));
-				++count;
-			}
-			++fila;
-		}
-		return true;
-	}
-
-	bool modificarVuelo(const Vuelo& v) {
-		Vuelo viejoVuelo;
-		if (!buscarVuelo(v.getId(), viejoVuelo))
-			return false;
-
-		repoVuelos.actualizar(v);
-		idx->eliminar(v.getId());
-		idx->insertar(v.getId(), v);
-
-		delete vuelosPorFecha;
-		vuelosPorFecha = new ArbolAVL<Vuelo>(insertarEnListaTemporal);
-		cargarIndice();
-
-		return true;
-	}
-
-	bool eliminarVuelo(int id) {
-		repoVuelos.eliminar(id);
-		idx->eliminar(id);
-		repoAsientos.eliminarPorVuelo(id);
-
-		delete vuelosPorFecha;
-		vuelosPorFecha = new ArbolAVL<Vuelo>(insertarEnListaTemporal);
-		cargarIndice();
-
-		return true;
-	}
+    bool crearVuelo(const Vuelo& v);
+    bool modificarVuelo(const Vuelo& v);
+    bool eliminarVuelo(int id);
+    bool buscarVuelo(int id, Vuelo& v) const;
 };
